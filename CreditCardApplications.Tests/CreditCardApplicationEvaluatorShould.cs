15933ef9b2627b2d7b9e9d4b246416f66b56dbb9
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-13 19:00:04 -0500

    first couple of tests, xunit, moq install

---
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+
+namespace CreditCardApplications.Tests
+{
+    public class CreditCardApplicationEvaluatorShould
+    {
+        [Fact]
+        public void AcceptHighIncomeApplications()
+        {
+            //Arrange
+            var sut = new CreditCardApplicationEvaluator(null);
+            var application = new CreditCardApplication { GrossAnnualIncome = 100_000 };
+
+            //Act
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            //Assert
+            Assert.Equal(CreditCardApplicationDecision.AutoAccepted, decision);
+        }
+
+        [Fact]
+        public void ReferYoungApplications()
+        {
+            //Arrange
+            var sut = new CreditCardApplicationEvaluator(null);
+            var application = new CreditCardApplication { Age = 19 };
+
+            //Act
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+
+            //Assert
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+
+    }
+}
