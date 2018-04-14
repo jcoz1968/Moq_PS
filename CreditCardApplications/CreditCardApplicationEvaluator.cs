4becbba4a3eb933bd2ac55b137a60a0287fc25cf
Jeffery Cosby <jcoz1968@hotmail.com>
2018-04-13 20:58:27 -0500

    properties, heirarchy, default value mocks

--- CreditCardApplications/CreditCardApplicationEvaluator.cs
+++ CreditCardApplications/CreditCardApplicationEvaluator.cs
@@ -14,42 +14,20 @@
 
         public CreditCardApplicationDecision Evaluate(CreditCardApplication application)
         {
-
             if (application.GrossAnnualIncome >= HighIncomeThreshhold)
             {
                 return CreditCardApplicationDecision.AutoAccepted;
             }
 
-            var isValidFrequentFlyerNumber = 
+            if (_validator.ServiceInformation.License.LicenseKey == "EXPIRED")
+            {
+                return CreditCardApplicationDecision.ReferredToHuman;
+            }
+
+            _validator.ValidationMode = application.Age >= 30 ? ValidationMode.Detailed : ValidationMode.Quick;
+
+            var isValidFrequentFlyerNumber =
                 _validator.IsValid(application.FrequentFlyerNumber);
-
-            if(!isValidFrequentFlyerNumber)
-            {
-                return CreditCardApplicationDecision.ReferredToHuman;
-            }
-
-            if (application.Age <= AutoReferralMaxAge)
-            {
-                return CreditCardApplicationDecision.ReferredToHuman;
-            }
-
-            if (application.GrossAnnualIncome < LowIncomeThreshhold)
-            {
-                return CreditCardApplicationDecision.AutoDeclined;
-            }
-
-            return CreditCardApplicationDecision.ReferredToHuman;
-        }
-
-        public CreditCardApplicationDecision EvaluateUsingOut(CreditCardApplication application)
-        {
-
-            if (application.GrossAnnualIncome >= HighIncomeThreshhold)
-            {
-                return CreditCardApplicationDecision.AutoAccepted;
-            }
-
-            _validator.IsValid(application.FrequentFlyerNumber, out var isValidFrequentFlyerNumber);
 
             if (!isValidFrequentFlyerNumber)
             {
@@ -68,6 +46,34 @@
 
             return CreditCardApplicationDecision.ReferredToHuman;
         }
+
+        //public CreditCardApplicationDecision EvaluateUsingOut(CreditCardApplication application)
+        //{
+
+        //    if (application.GrossAnnualIncome >= HighIncomeThreshhold)
+        //    {
+        //        return CreditCardApplicationDecision.AutoAccepted;
+        //    }
+
+        //    _validator.IsValid(application.FrequentFlyerNumber, out var isValidFrequentFlyerNumber);
+
+        //    if (!isValidFrequentFlyerNumber)
+        //    {
+        //        return CreditCardApplicationDecision.ReferredToHuman;
+        //    }
+
+        //    if (application.Age <= AutoReferralMaxAge)
+        //    {
+        //        return CreditCardApplicationDecision.ReferredToHuman;
+        //    }
+
+        //    if (application.GrossAnnualIncome < LowIncomeThreshhold)
+        //    {
+        //        return CreditCardApplicationDecision.AutoDeclined;
+        //    }
+
+        //    return CreditCardApplicationDecision.ReferredToHuman;
+        //}
 
 
     }
